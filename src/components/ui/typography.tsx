3aa106759386ae271941a85786c488ebf88c9011
kobyrthr <kobyrthr@gmail.com>
2025-02-17 15:32:25 +0800

    Add typography

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+export const typography = cva("text-foreground", {
+    variants: {
+        type: {
+            "heading-l": "font-bold text-[36px] leading-[33px] tracking-[-1px]",
+            "heading-m": "font-bold text-[24px] leading-[22px] tracking-[-0.75px]",
+            "heading-s": "font-bold text-[15px] leading-[24px] tracking-[-0.25px]",
+            "heading-s-variant": "font-bold text-[15px] leading-[15px] tracking-[-0.25px]",
+            body: "font-medium text-[13px] leading-[18px] tracking-[-0.1px]",
+            "body-variant": "font-medium text-[13px] leading-[15px] tracking-[-0.25px]",
+        },
+    },
+    defaultVariants: {
+        type: "body",
+    },
+})
+
+export interface TypographyProps extends VariantProps<typeof typography> {
+    children: React.ReactNode
+    className?: string
+    asChild?: boolean
+}
+
+export const Typography = ({
+    children,
+    type,
+    className,
+    asChild,
+    ...props
+}: TypographyProps) => {
+    const Comp = asChild ? Slot : "p"
+    return (
+        <Comp className={cn(typography({ type, className }))} {...props}>
+            {children}
+        </Comp>
+    )
+}
