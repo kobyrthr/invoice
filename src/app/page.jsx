fbe952d16fab97b6cafac861553ed1c650953263
kobyrthr <kobyrthr@gmail.com>
2025-02-20 14:45:45 +0800

    Add responsive navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,88 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Typography } from '@/components/ui/typography';
+import { SelectSeparator } from '@radix-ui/react-select';
+import { PlusCircle } from '@phosphor-icons/react';
+import { Fragment } from 'react';
+import { useIsMobile } from '@/hooks/use-mobile';
+
 export default function Home() {
+  const invoices = [1, 2, 3, 4];
+  const statuses = ['Pending', 'Paid', 'Draft'];
+  const isMobile = useIsMobile();
+
   return (
-    <div className="grid bg-background items-center justify-items-center size-full p-8 pb-20 gap-16 sm:p-20 font-spartan">
-      <div className="size-20 bg-black">fasdfs</div>
-      <div className="size-20 bg-black">fasdfs</div>
-      <div className="size-20 bg-black">fasdfs</div>
+    <div className=" grid bg-background items-center justify-items-center size-full p-8 pb-20 gap-16 sm:p-20">
+      <div className="size-full flex flex-col gap-8 min-[376px]:gap-14 md:gap-16 max-w-screen-md">
+        <div className="flex justify-between w-full">
+          <div className="">
+            <Typography asChild type="heading-l">
+              <h1>Invoices</h1>
+            </Typography>
+            <Typography type="body" className="text-color-07">
+              {isMobile
+                ? `${invoices.length} invoices`
+                : `There are ${invoices.length} total invoices`}
+            </Typography>
+          </div>
+
+          <div className="flex gap-4 md:gap-20">
+            <Select>
+              <SelectTrigger
+                defaultValue="net-1"
+                className=" w-14 md:w-[118px] border-none bg-transparent p-0 normal-case"
+              >
+                <SelectValue
+                  placeholder={isMobile ? 'Filter' : 'Filter by status'}
+                />
+              </SelectTrigger>
+              <SelectContent
+                style={{ width: 'var(--radix-select-trigger-width)' }}
+                sideOffset={24}
+              >
+                {statuses.map((item, index) => (
+                  <Fragment key={item}>
+                    {index > 0 ? <SelectSeparator /> : null}
+                    <SelectItem className="capitalize" value={item}>
+                      {item}
+                    </SelectItem>
+                  </Fragment>
+                ))}
+              </SelectContent>
+            </Select>
+            <Button
+              variant="primary"
+              className="h-fit gap-4 p-2 pr-4"
+              IconLeft={() => (
+                <PlusCircle
+                  height={24}
+                  width={24}
+                  weight="fill"
+                  className="text-white min-h-8 min-w-8"
+                />
+              )}
+            >
+              <span className="pt-0.5">{isMobile ? 'New' : 'New Invoice'}</span>
+            </Button>
+          </div>
+        </div>
+        <Card>
+          <CardContent>
+            <Typography asChild type="heading-l">
+              <h1>Invoices</h1>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/components/ui/typography.jsx
+++ src/components/ui/typography.jsx
@@ -1,35 +1,36 @@
-import { cn } from "@/lib/utils"
-import { Slot } from "@radix-ui/react-slot"
-import { cva} from "class-variance-authority"
+import { cn } from '@/lib/utils';
+import { Slot } from '@radix-ui/react-slot';
+import { cva } from 'class-variance-authority';
 
-export const typography = cva("text-foreground", {
-    variants: {
-        type: {
-            "heading-l": "font-bold text-[36px] leading-[33px] tracking-[-1px]",
-            "heading-m": "font-bold text-[24px] leading-[22px] tracking-[-0.75px]",
-            "heading-s": "font-bold text-[15px] leading-[24px] tracking-[-0.25px]",
-            "heading-s-variant": "font-bold text-[15px] leading-[15px] tracking-[-0.25px]",
-            body: "font-medium text-[13px] leading-[18px] tracking-[-0.1px]",
-            "body-variant": "font-medium text-[13px] leading-[15px] tracking-[-0.25px]",
-        },
+export const typography = cva('text-foreground', {
+  variants: {
+    type: {
+      'heading-l': 'font-bold text-[36px] leading-[33px] tracking-[-1px]',
+      'heading-m': 'font-bold text-[24px] leading-[22px] tracking-[-0.75px]',
+      'heading-s': 'font-bold text-[15px] leading-[24px] tracking-[-0.25px]',
+      'heading-s-variant':
+        'font-bold text-[15px] leading-[15px] tracking-[-0.25px]',
+      body: 'font-medium text-[13px] leading-[18px] tracking-[-0.1px]',
+      'body-variant':
+        'font-medium text-[13px] leading-[15px] tracking-[-0.25px]',
     },
-    defaultVariants: {
-        type: "body",
-    },
-})
-
+  },
+  defaultVariants: {
+    type: 'body',
+  },
+});
 
 export const Typography = ({
-    children,
-    type,
-    className,
-    asChild,
-    ...props
+  children,
+  type,
+  className,
+  asChild,
+  ...props
 }) => {
-    const Comp = asChild ? Slot : "p"
-    return (
-        <Comp className={cn(typography({ type, className }))} {...props}>
-            {children}
-        </Comp>
-    )
-}
+  const Comp = asChild ? Slot : 'p';
+  return (
+    <Comp className={cn(typography({ type, className }))} {...props}>
+      {children}
+    </Comp>
+  );
+};
