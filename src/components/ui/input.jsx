1bf6bb5937b7a7527ef5ba73a45044b1e65a39fe
kobyrthr <kobyrthr@gmail.com>
2025-02-20 14:46:20 +0800

    format codebase with prettier

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,22 +1,20 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-const Input = React.forwardRef(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-12 w-full rounded-md bg-white px-5 py-3.5 text-[15px] font-bold tracking-[-0.25px] shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-color-08 border border-color-05 focus-visible:!border-color-02 focus-visible:outline-none",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        'flex h-12 w-full rounded-md bg-white px-5 py-3.5 text-[15px] font-bold tracking-[-0.25px] shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-color-08 border border-color-05 focus-visible:!border-color-02 focus-visible:outline-none',
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = 'Input';
 
-export { Input }
+export { Input };
--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,21 +1,22 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva} from "class-variance-authority"
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+import { cva } from 'class-variance-authority';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", {
-  variants: {
-    variant: {
-      default: "",
-      "form-label": "!text-color-07 font-medium text-[13px]",
+  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+  {
+    variants: {
+      variant: {
+        default: '',
+        'form-label': '!text-color-07 font-medium text-[13px]',
+      },
     },
-  },
-}
-)
+  }
+);
 
 const Label = React.forwardRef(({ className, variant, ...props }, ref) => (
   <LabelPrimitive.Root
@@ -23,7 +24,7 @@ const Label = React.forwardRef(({ className, variant, ...props }, ref) => (
     className={cn(labelVariants({ variant }), className)}
     {...props}
   />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+));
+Label.displayName = LabelPrimitive.Root.displayName;
 
-export { Label }
+export { Label };
