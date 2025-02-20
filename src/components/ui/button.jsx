0979f73ce1608f08765c89f6531e3907577f269d
kobyrthr <kobyrthr@gmail.com>
2025-02-20 14:46:20 +0800

    format codebase with prettier

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,53 +1,57 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva } from "class-variance-authority"
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva } from 'class-variance-authority';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-color-sm font-bold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-color-sm font-bold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        primary:
-          "bg-color-01 text-white hover:bg-color-02",
-        default:
-          "bg-color-07-base text-color-07 hover:bg-color-05",
-        secondary:
-          "bg-color-06-base text-color-06 hover:bg-color-06-hover",
-        destructive:
-          "bg-color-09 text-white hover:bg-color-10",
-        link: "text-color-01 underline-offset-4 hover:underline",
+        primary: 'bg-color-01 text-white hover:bg-color-02',
+        default: 'bg-color-07-base text-color-07 hover:bg-color-05',
+        secondary: 'bg-color-06-base text-color-06 hover:bg-color-06-hover',
+        destructive: 'bg-color-09 text-white hover:bg-color-10',
+        link: 'text-color-01 underline-offset-4 hover:underline',
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-color-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9 gap-3 px-2 py-2 sm:gap-4",
+        default: 'h-9 px-4 py-2',
+        sm: 'h-8 rounded-md px-3 text-color-xs',
+        lg: 'h-10 rounded-md px-8',
+        icon: 'h-9 w-9 gap-3 px-2 py-2 sm:gap-4',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
   }
-)
-
+);
 
 const Button = React.forwardRef(
-  ({ IconLeft, className, variant, size, asChild = false, children, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+  (
+    { IconLeft, className, variant, size, asChild = false, children, ...props },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
-        {IconLeft ? <><IconLeft /> {children}</> : children}
+        {IconLeft ? (
+          <>
+            <IconLeft /> {children}
+          </>
+        ) : (
+          children
+        )}
       </Comp>
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = 'Button';
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -1,89 +1,93 @@
-"use client";
-import * as React from "react"
-import { DayPicker } from "react-day-picker"
+'use client';
+import * as React from 'react';
+import { DayPicker } from 'react-day-picker';
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import Image from "next/image"
-import IconArrowLeft from "@/../public/icon-arrow-left.svg"
-import IconArrowRight from "@/../public/icon-arrow-right.svg"
-import { format } from "date-fns" 
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
+import IconArrowLeft from '@/../public/icon-arrow-left.svg';
+import IconArrowRight from '@/../public/icon-arrow-right.svg';
+import { format } from 'date-fns';
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }) {
   const formatters = {
-        formatCaption: (date, options) => {
-            return format(date, "MMM yyyy", { locale: options?.locale });
-        },
-        formatMonthCaption: (date, options) => {
-            return format(date, "MMMM", { locale: options?.locale });
-        },
-        formatYearCaption: (date, options) => {
-            return format(date, "yyyy", { locale: options?.locale });
-        },
-    };
+    formatCaption: (date, options) => {
+      return format(date, 'MMM yyyy', { locale: options?.locale });
+    },
+    formatMonthCaption: (date, options) => {
+      return format(date, 'MMMM', { locale: options?.locale });
+    },
+    formatYearCaption: (date, options) => {
+      return format(date, 'yyyy', { locale: options?.locale });
+    },
+  };
   return (
-    (<DayPicker
+    <DayPicker
       showOutsideDays={showOutsideDays}
       formatters={formatters}
       fixedWeeks
-      className={cn("p-4 py-6", className)}
+      className={cn('p-4 py-6', className)}
       hideHead
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-regular",
-        nav: "space-x-1 flex items-center",
+        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
+        month: 'space-y-4',
+        caption: 'flex justify-center pt-1 relative items-center',
+        caption_label: 'text-regular',
+        nav: 'space-x-1 flex items-center',
         nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 hover:opacity-80"
+          buttonVariants({ variant: 'outline' }),
+          'h-7 w-7 bg-transparent p-0 hover:opacity-80'
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        nav_button_previous: 'absolute left-1',
+        nav_button_next: 'absolute right-1',
+        table: 'w-full border-collapse space-y-1',
+        head_row: 'flex',
         head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
+          'text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]',
+        row: 'flex w-full mt-2',
         cell: cn(
-          "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-transparent [&:has([aria-selected].day-outside)]:bg-transparent [&:has([aria-selected].day-range-end)]:rounded-r-md",
-          props.mode === "range"
-            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-            : "[&:has([aria-selected])]:rounded-md"
+          'relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-transparent [&:has([aria-selected].day-outside)]:bg-transparent [&:has([aria-selected].day-range-end)]:rounded-r-md',
+          props.mode === 'range'
+            ? '[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md'
+            : '[&:has([aria-selected])]:rounded-md'
         ),
         day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "size-[30px] p-0 h-fit text-regular aria-selected:opacity-100 hover:text-color-01/80"
+          buttonVariants({ variant: 'ghost' }),
+          'size-[30px] p-0 h-fit text-regular aria-selected:opacity-100 hover:text-color-01/80'
         ),
-        day_range_start: "day-range-start",
-        day_range_end: "day-range-end",
-        day_selected:
-          "text-color-01 hover:text-color-08 focus:text-color-01",
-        day_today: "text-color-08",
-        day_outside:
-          "day-outside text-color-08/10",
-        day_disabled: "text-color-08/10",
+        day_range_start: 'day-range-start',
+        day_range_end: 'day-range-end',
+        day_selected: 'text-color-01 hover:text-color-08 focus:text-color-01',
+        day_today: 'text-color-08',
+        day_outside: 'day-outside text-color-08/10',
+        day_disabled: 'text-color-08/10',
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
-        day_hidden: "invisible",
+          'aria-selected:bg-accent aria-selected:text-accent-foreground',
+        day_hidden: 'invisible',
         ...classNames,
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
-          <Image src={IconArrowLeft} alt="arrow-left" className={cn(className)} {...props} />
+          <Image
+            src={IconArrowLeft}
+            alt="arrow-left"
+            className={cn(className)}
+            {...props}
+          />
         ),
         IconRight: ({ className, ...props }) => (
-          <Image src={IconArrowRight} alt="arrow-right" className={cn(className)} {...props} />
+          <Image
+            src={IconArrowRight}
+            alt="arrow-right"
+            className={cn(className)}
+            {...props}
+          />
         ),
       }}
-      {...props} />)
+      {...props}
+    />
   );
 }
-Calendar.displayName = "Calendar"
+Calendar.displayName = 'Calendar';
 
-export { Calendar }
+export { Calendar };
