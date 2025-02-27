a9ab7dba3fd4671921ff79c3fffb231ff0020ea1
kobyrthr <kobyrthr@gmail.com>
2025-02-27 15:20:58 +0800

    Add invoice create

--- src/app/client-layout.jsx
+++ src/app/client-layout.jsx
@@ -8,6 +8,29 @@ export default function ClientLayout({ children }) {
   const [selectedStatuses, setSelectedStatuses] = useState(null);
   const [invoice, setInvoice] = useState(null);
   const [invoices, setInvoices] = useState(DATA);
+  const addInvoice = (inv) => {
+    const generateId = () => {
+      const chars =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let id = '';
+      for (let i = 0; i < 6; i++) {
+        id += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return id;
+    };
+
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}-${String(
+      today.getMonth() + 1
+    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+
+    const newInvoice = {
+      ...inv,
+      id: generateId().toUpperCase(),
+      createdAt: formattedDate,
+    };
+    setInvoices((invoices) => [newInvoice, ...invoices]);
+  };
   const updateInvoice = (inv) => {
     const newInvoice = { ...invoice, ...inv };
 
@@ -32,6 +55,7 @@ export default function ClientLayout({ children }) {
         statuses,
         selectedStatuses,
         setSelectedStatuses,
+        addInvoice,
         updateInvoice,
         deleteInvoice,
       }}
--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     <input
-      type={type}
+      type={type ?? 'text'}
       className={cn(
         'flex h-12 w-full rounded-md bg-white px-5 py-3.5 text-[15px] font-bold tracking-[-0.25px] shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-color-08 border border-color-05 focus-visible:!border-color-02 focus-visible:outline-none',
         className
