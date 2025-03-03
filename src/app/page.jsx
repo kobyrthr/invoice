ba3cb9ea4ad98beb531bdff227ad23489434e5dd
kobyrthr <kobyrthr@gmail.com>
2025-03-03 15:30:13 +0800

    fix padding

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     ? invoices.filter((invoice) => selectedStatuses?.includes(invoice?.status))
     : invoices;
   return (
-    <div className=" grid bg-background items-center justify-items-center size-full max-[368px]:p-6 p-12 max-[368px]:pb-10 pb-20 gap-16 lg:p-20">
+    <div className=" grid bg-background items-center justify-items-center size-full max-[376px]:p-6 p-12 max-[376px]:pb-10 pb-20 gap-16 lg:p-20">
       <div className="size-full flex flex-col gap-8 min-[376px]:gap-14 lg:gap-16 max-w-screen-md">
         <PageHeader />
 
